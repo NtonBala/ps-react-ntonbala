0f946af5f3611300a7471110defdf7db76dadf43
NtonBala <balashov.anton.1983@gmail.com>
2022-10-28 12:13:12 +0300

    Enhance Label examples

---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,6 @@
+import Label from 'ps-reusable-react-components-ntonbala/Label';
+
+/** Required label */
+const ExampleRequired = () => <Label htmlFor="test" label="test" required />;
+
+export default ExampleRequired;
