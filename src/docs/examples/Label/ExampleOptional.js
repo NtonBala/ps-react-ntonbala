2587044dfd9c6f315c782f354eb6361c96cf07b4
NtonBala <balashov.anton.1983@gmail.com>
2022-10-28 12:07:45 +0300

    Add Label atom

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,6 @@
+import Label from 'ps-reusable-react-components-ntonbala/Label';
+
+/** Optional label */
+const ExampleOptional = () => <Label htmlFor="test" label="test" />;
+
+export default ExampleOptional;
