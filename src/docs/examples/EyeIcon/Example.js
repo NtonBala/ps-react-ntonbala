cb2a66289e56d0788b5edb4f3d9721e8b3fdc0db
NtonBala <balashov.anton.1983@gmail.com>
2022-10-30 11:53:45 +0200

    Add currentColor support to EyeIcon atom

--- src/docs/examples/EyeIcon/Example.js
+++ src/docs/examples/EyeIcon/Example.js
@@ -1,5 +1,6 @@
 import EyeIcon from 'ps-reusable-react-components-ntonbala/EyeIcon';
 
+/** Default Eye Icon */
 const ExampleEyeIcon = () => <EyeIcon />;
 
 export default ExampleEyeIcon;
---
+++ src/docs/examples/EyeIcon/ExampleColored.js
@@ -0,0 +1,11 @@
+import EyeIcon from 'ps-reusable-react-components-ntonbala/EyeIcon';
+
+/** Colored Eye Icons */
+const ExampleEyeIconsColored = () => (
+  <div style={{ color: 'green' }}>
+    <EyeIcon />
+    <EyeIcon style={{ color: 'black' }} />
+  </div>
+);
+
+export default ExampleEyeIconsColored;
